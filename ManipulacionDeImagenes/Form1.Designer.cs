56e235ceed57a88aa86fff8abd6684653cef54a6
alejandra sanchez barrera <A310401@alumnos.uaslp.mx>
2021-11-30 23:00:35 -0600

    first commit

---
+++ ManipulacionDeImagenes/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace ManipulacionDeImagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.original = new System.Windows.Forms.Button();
+            this.invertir = new System.Windows.Forms.Button();
+            this.gris = new System.Windows.Forms.Button();
+            this.bw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // original
+            // 
+            this.original.BackColor = System.Drawing.Color.Thistle;
+            this.original.Location = new System.Drawing.Point(13, 13);
+            this.original.Name = "original";
+            this.original.Size = new System.Drawing.Size(75, 37);
+            this.original.TabIndex = 0;
+            this.original.Text = "Original";
+            this.original.UseVisualStyleBackColor = false;
+            this.original.MouseClick += new System.Windows.Forms.MouseEventHandler(this.original_MouseClick);
+            // 
+            // invertir
+            // 
+            this.invertir.BackColor = System.Drawing.Color.Lime;
+            this.invertir.Location = new System.Drawing.Point(100, 13);
+            this.invertir.Name = "invertir";
+            this.invertir.Size = new System.Drawing.Size(75, 37);
+            this.invertir.TabIndex = 1;
+            this.invertir.Text = "Invertir";
+            this.invertir.UseVisualStyleBackColor = false;
+            this.invertir.MouseClick += new System.Windows.Forms.MouseEventHandler(this.invertir_MouseClick);
+            // 
+            // gris
+            // 
+            this.gris.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.gris.Location = new System.Drawing.Point(308, 13);
+            this.gris.Name = "gris";
+            this.gris.Size = new System.Drawing.Size(75, 37);
+            this.gris.TabIndex = 3;
+            this.gris.Text = "Grises";
+            this.gris.UseVisualStyleBackColor = false;
+            this.gris.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gris_MouseClick);
+            // 
+            // bw
+            // 
+            this.bw.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bw.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bw.Location = new System.Drawing.Point(187, 13);
+            this.bw.Name = "bw";
+            this.bw.Size = new System.Drawing.Size(109, 37);
+            this.bw.TabIndex = 2;
+            this.bw.Text = "Blanco/Negro";
+            this.bw.UseVisualStyleBackColor = false;
+            this.bw.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bw_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 603);
+            this.Controls.Add(this.gris);
+            this.Controls.Add(this.bw);
+            this.Controls.Add(this.invertir);
+            this.Controls.Add(this.original);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button original;
+        private System.Windows.Forms.Button invertir;
+        private System.Windows.Forms.Button gris;
+        private System.Windows.Forms.Button bw;
+    }
+}
+
---
+++ ManipulacionDeImagenes/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipulacionDeImagenes
+{
+    public partial class Form1 : Form
+    {
+        Image img,img2;
+        Bitmap bmp,bmp2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            img=Image.FromFile(@"D:\alexa\UASLP\Semestre V (2021-2022)\Interfaces gráficas\VisualStudio\ManipulacionDeImagenes\hojas.jpg");
+            img2 = Image.FromFile(@"D:\alexa\UASLP\Semestre V (2021-2022)\Interfaces gráficas\VisualStudio\ManipulacionDeImagenes\hojas.jpg");
+            bmp = (Bitmap)img;
+            bmp2 =(Bitmap)img2;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bmp, 100, 55);
+        }
+
+        private void original_MouseClick(object sender, MouseEventArgs e)
+        {
+            Color c;
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    c = bmp2.GetPixel(x, y);
+
+                    bmp.SetPixel(x, y, c);
+                }
+            }
+
+            Invalidate();
+        }
+
+        private void invertir_MouseClick(object sender, MouseEventArgs e)
+        {
+            Color c,nc;
+
+            for(int y = 0; y < bmp.Height; y++) 
+            {
+                for (int x = 0; x < bmp.Width; x++) 
+                {
+                    c=bmp.GetPixel(x, y);
+
+                    nc = Color.FromArgb(c.A,255-c.R,255-c.G,255-c.B);
+
+                    bmp.SetPixel(x, y, nc);
+                }
+            }
+
+            Invalidate(); //le dice al SO que mande un mensaje de que se tiene que redibujar
+        }
+
+        private void bw_MouseClick(object sender, MouseEventArgs e)
+        {
+            Color c, nc;
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    c = bmp.GetPixel(x, y);
+                    byte gray = (byte)(c.R * 0.3f + c.G * 0.59f + c.B * 0.11f);
+                    byte value = 0;
+                    if (gray > 100)
+                    {
+                        value = 255;
+                    }
+                    nc = Color.FromArgb(c.A, value, value, value);
+
+                    bmp.SetPixel(x, y, nc);
+                }
+            }
+
+            Invalidate();
+        }
+
+        private void gris_MouseClick(object sender, MouseEventArgs e)
+        {
+            Color c, nc;
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    c = bmp.GetPixel(x, y);
+                    int intensidad = (c.R + c.G + c.B) / 3;
+                    nc = Color.FromArgb(c.A, intensidad, intensidad, intensidad);
+
+                    bmp.SetPixel(x, y, nc);
+                }
+            }
+
+            Invalidate();
+        }
+    }
+}
